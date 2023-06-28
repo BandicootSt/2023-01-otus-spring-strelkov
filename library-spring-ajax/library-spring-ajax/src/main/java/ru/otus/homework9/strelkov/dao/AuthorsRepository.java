399cd6ac3d15cfedf39988d557cd992a48cdada1
strelkov <strelkov@ofd.ru>
2023-06-28 14:54:50 +0300

    Revert "Strelkov, otus-spring-2023-01, homework-9"
    This reverts commit 1837b58e2ed95fbb39ac458a4a1f062b18f3e788.

--- library-spring-ajax/library-spring-ajax/src/main/java/ru/otus/homework9/strelkov/dao/AuthorsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.homework9.strelkov.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.otus.homework9.strelkov.domain.Author;
-
-public interface AuthorsRepository extends JpaRepository<Author, Long> {
-}
